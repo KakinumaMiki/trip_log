84f215bd486ff6c435e3334880965f961f9747b3
Kakinuma Miki <78365241+KakinumaMiki@users.noreply.github.com>
2022-10-11 19:35:37 +0900

    日本時間、言語の設定

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,13 @@ module TripLog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Asia/Tokyo'
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'model', '*.{rb,yml}').to_s]
+
     config.generators do |g|
       g.assets false # CSS, JavaScriptファイル生成せず
       g.helper false # ヘルパーを作らない
